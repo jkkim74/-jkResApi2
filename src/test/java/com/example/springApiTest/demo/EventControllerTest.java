9161709d63074278a5da7ab3dc79051333ab2f88
jkkim74 <jack68@naver.com>
2021-03-01 00:29:14 +0900

    save function add

--- src/test/java/com/example/springApiTest/demo/EventControllerTest.java
+++ src/test/java/com/example/springApiTest/demo/EventControllerTest.java
@@ -2,11 +2,14 @@ package com.example.springApiTest.demo;
 
 
 import com.example.springApiTest.demo.events.Event;
+import com.example.springApiTest.demo.events.EventRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -30,6 +33,9 @@ public class EventControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEventTest() throws Exception {
         Event event = Event.builder()
@@ -44,6 +50,8 @@ public class EventControllerTest {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타팀 팩토리")
                 .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
