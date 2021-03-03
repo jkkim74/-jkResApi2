8f93851463cf46d9c9e9c124ae06fab0cc51fc23
jkkim74 <jack68@naver.com>
2021-03-04 06:27:00 +0900

    unknown properties add

--- src/test/java/com/example/springApiTest/demo/EventControllerTest.java
+++ src/test/java/com/example/springApiTest/demo/EventControllerTest.java
@@ -2,13 +2,18 @@ package com.example.springApiTest.demo;
 
 
 import com.example.springApiTest.demo.events.Event;
+import com.example.springApiTest.demo.events.EventDto;
 import com.example.springApiTest.demo.events.EventRepository;
+import com.example.springApiTest.demo.events.EventStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -25,7 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
 
     @Autowired
@@ -34,15 +40,9 @@ public class EventControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
-
-    @Autowired
-    private WebApplicationContext wac;
-
     @Test
     public void createEventTest() throws Exception {
-        Event event = Event.builder()
+        EventDto event = EventDto.builder()
                 .name("Spring")
                 .description("REST Api Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2021, 03, 01, 13, 00))
@@ -54,8 +54,6 @@ public class EventControllerTest {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타팀 팩토리")
                 .build();
-        event.setId(10);
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
         mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON+";charset=utf-8")
                 .accept(MediaTypes.HAL_JSON)
@@ -64,7 +62,42 @@ public class EventControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists("location"))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE+";charset=utf-8"));
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE+";charset=utf-8"))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(Matchers.is("DRAFT")));
     }
+
+
+    @Test
+    public void createEventTest_Bad_Request() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .name("Spring")
+                .description("REST Api Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 03, 01, 13, 00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 03, 05, 23, 59))
+                .beginEventDateTime(LocalDateTime.of(2021, 03, 6, 00, 01))
+                .endEventDateTime(LocalDateTime.of(2021, 03, 9, 23, 59))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타팀 팩토리")
+                .free(true)
+                .offline(false)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON+";charset=utf-8")
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+        ;
+    }
+
+
+
+
 
 }
